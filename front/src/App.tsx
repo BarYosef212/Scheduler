64a9ae01e22ddcd4108813497aadd3c7d1206837
Bar Yosef Tayar Mac <bartayar123456789@gmail.com>
2025-03-01 16:54:50 +0200

    initialize project

---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Main from './components/Main/main'
+
+function App() {
+
+  return (
+    <>
+      <Main/>
+    </>
+  )
+}
+
+export default App
---
+++ front/src/components/Main/Main.tsx
@@ -0,0 +1,13 @@
+import './Main.css'
+
+const Main: React.FC = () => {
+  return (
+    <main className='main-page'>
+     <h1 className="main-main-label">ARIEL HAIR STYLE</h1> 
+     <h4>ספר גברים, ניתן לקבוע תור כעת</h4>
+     <button className='btn main-main-btn'>קבע תור</button>
+    </main>
+  );
+};
+
+export default Main;
